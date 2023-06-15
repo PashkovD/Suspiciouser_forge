bdb55bb4725613b1b8c288b6b6bef00e60b04d20
PashkovD <dimon2006763@gmail.com>
2023-06-15 12:35:05 +0500

    Initial commit

---
+++ src/main/java/ru/pashkovd/suspiciouser/Suspiciouser_forge.java
@@ -0,0 +1,9 @@
+package ru.pashkovd.suspiciouser;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Suspiciouser_forge.MODID)
+public class Suspiciouser_forge {
+    public static final String MODID = "suspiciouser";
+
+}
